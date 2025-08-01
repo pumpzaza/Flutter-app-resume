209e670b9271b1369fb6b0ca4f4fb5f0c47d6262
pumpzaza <nattapong4954@gmail.com>
2025-08-01 22:25:58 +0700

    Resume66311735

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // Import ไลบรารี Material ของ Flutter
 
 void main() {
-  // จุดเริ่มต้นของแอป Flutter
-  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
+  runApp(const MyApp());
 }
 
-// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // Constructor
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
-    return MaterialApp(
-      title: 'My Awesome App',
-      debugShowCheckedModeBanner: false, // ซ่อนแบนเนอร์ debug
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 250, 251, 217),
-        ),
-        useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
-      ),
-      home: const MyHomePage(), // วิดเจ็ตหน้าจอหลักของคุณ
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyProfilePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyProfilePage extends StatelessWidget {
+  const MyProfilePage({super.key});
 
-  // ภายในเมธอด build ของ MyHomePage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chapter 4'),
-        backgroundColor: const Color.fromARGB(255, 241, 170, 237),
+        title: const Text('ประวัติส่วนตัว'),
       ),
       body: Padding(
-        // เพิ่ม Padding รอบ Column
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/250px-Tour_Eiffel_Wikimedia_Commons.jpg'),
-            const SizedBox(height: 16.0),
-            Image.asset(
-              'asset/img/ai-generated-pic-artistic-depiction-of-sunflowers-under-a-vast-cloudy-sky-photo.jpg',
-              errorBuilder: (context, error, stackTrace) {
-                return const Center(
-                  child: Text(
-                    'ไม่สามารถโหลดรูปภาพได้',
-                    style: TextStyle(color: Colors.red),
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              alignment: Alignment.center,
-              width: 600.0,
-              height: 50.0,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 121, 188, 215),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Text(
-                "สวัสดี Flutter",
-                style: GoogleFonts.lato(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(221, 8, 58, 128),
-                ),
+        child: ListView(
+          children: const [
+            Center(
+              child: CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('asset/img/1000003803.jpg'),
               ),
             ),
-            const SizedBox(height: 10.0),
-            Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    print('กดปุ่ม Elevated');
-                  },
-                  child: const Text('Elevated'),
-                ),
-                const SizedBox(width: 10.0),
-                OutlinedButton(
-                  onPressed: () {
-                    print('กดปุ่ม Outlined');
-                  },
-                  child: const Icon(Icons.person),
-                ),
-                const SizedBox(width: 10.0),
-                TextButton(
-                  onPressed: () {
-                    print('กดปุ่ม Text');
-                  },
-                  child: const Text('Text'),
-                ),
-              ],
-            ),
-            IconButton(
-              onPressed: () {
-                print('กดไอคอน Info');
-              },
-              icon: const Icon(Icons.info_outline),
-              iconSize: 30,
-              tooltip: 'ข้อมูล',
-              color: const Color.fromARGB(255, 124, 124, 128),
-            ),
+            SizedBox(height: 20),
+            Text("ชื่อ - นามสกุล: ณัฐพงศ์ อินทรารุจิกุล"),
+            Text("ภูมิลำเนา: จังหวัดแพร่"),
+            Text("งานอดิเรก: เล่นเกม"),
+            SizedBox(height: 20),
+            Text("ประวัติการศึกษา"),
+            SizedBox(height: 10),
+            Text("ประถมศึกษา: โรงเรียนเมธังกราวาส (จบปี 2559)"),
+            Text("มัธยมต้น: โรงเรียนพิริยาลัย (จบปี 2554)"),
+            Text("มัธยมปลาย: โรงเรียนพิริยาลัย (จบปี 2565)"),
           ],
         ),
       ),
